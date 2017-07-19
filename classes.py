964eb8c4429f132683d54a99eb5a3d2f80eafa80
dmorgun <daryamorgun@gmail.com>
2017-07-19 10:18:59 +0300

    Modified classes.py

---
+++ classes.py
@@ -0,0 +1,69 @@
+# https://python.swaroopch.com/oop.html
+
+class Person:
+
+    def __init__ (self, name):
+        self.name = name
+
+    def say_hi(self):
+        print "Hello, my name is", self.name
+
+p = Person('Daria')
+p.say_hi()
+
+# The previous 2 lines can also be written as
+# Person('Daria').say_hi()
+
+class Robot:
+    """Represents a Robot, with a name"""
+    # A class variable, counting the number of robots
+    population = 0
+
+    def __init__ (self, name):
+        """Initializes the data"""
+        self.name = name
+        print "(Initializing {})".format(self.name)
+        # When this person is created, the robots
+        # adds to the population
+        Robot.population += 1
+
+    def die (self):
+        """I am dying."""
+        print ("{} is being destroyed!".format(self.name))
+
+        Robot.population -= 1
+
+        if Robot.population == 0:
+            print ("{} was the last one.".format(self.name))
+        else:
+            print ("There is still {:d} robots working".format(
+            Robot.population))
+
+    def say_hi(self):
+        """Greeting by the robot.
+
+        Yeah they can do that."""
+
+        print ("Greeting, my masters call me {}").format(self.name)
+
+    @classmethod
+    def how_many(cls):
+        """Prints the current population"""
+
+        print ("We have {:d} robots.".format(cls.population))
+
+droid1 = Robot("R2-D2")
+droid1.say_hi()
+Robot.how_many()
+
+droid2 = Robot("C-3PO")
+droid2.say_hi()
+Robot.how_many()
+
+print ("\nRobots can do some work here.\n")
+
+print ("\nRobots have finished their work. Let's destroy them.\n")
+droid1.die()
+droid2.die()
+
+Robot.how_many()
