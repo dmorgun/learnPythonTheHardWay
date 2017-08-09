bc36afca0ad22962581bc4578cf0d19ed6867569
dmorgun <daryamorgun@gmail.com>
2017-08-09 18:52:03 +0300

    import example

---
+++ import-example.py
@@ -0,0 +1,16 @@
+# Create package in PyCharm which contains empty __init__ file for import to work
+
+
+from src.config.brand_specific_config import *
+
+import src.config.brand_specific_config as gb
+
+gb.otf_brand_name
+
+src.config.brand_specific_config.otf_brand_na]
+print otf_brand_name
+print hello.otf_brand_name
+
+
+import pytest
+import uuid
